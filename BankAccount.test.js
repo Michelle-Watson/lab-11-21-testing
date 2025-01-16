81f0860fd4537a652eef79d20a75ecf9e9eb421b
Michelle-Watson <alicerockshard@gmail.com>
2025-01-15 21:55:37 -0500

    Q1. Test: Verify can deposit money into account

--- BankAccount.test.js
+++ BankAccount.test.js
@@ -3,7 +3,7 @@ import BankAccount from "./BankAccount.js";
 
 // SLIDE 12: Test Structure
 describe("BankAccount", () => {
-  it("is created with correct details", () => {
+  it("is created with correct details (Q1)", () => {
     // Arrange: Set up an initial state
     const account = new BankAccount("1234");
 
@@ -16,5 +16,16 @@ describe("BankAccount", () => {
     expect(account.balance).toEqual(0);
     // Verify that the account's PIN is the same as the pin value passed in to the constructor.
     expect(account.pin).toEqual("1234");
+  });
+
+  it("can deposit money (Q2)", () => {
+    // Arrange: Create an account with PIN '1234'.
+    const account = new BankAccount("1234");
+
+    // Act: Deposit 100 into the account.
+    account.deposit(100);
+
+    // Assert: Check if the balance is now 100.
+    expect(account.balance).toEqual(100);
   });
 });
